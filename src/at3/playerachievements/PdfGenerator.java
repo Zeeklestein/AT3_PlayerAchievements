5d339b25cb74e6f1192223258d4f57f0f8f100bb
Zeeklestein <bszekely91@gmail.com>
2020-05-28 09:29:06 +1000

    update pdfGenerator

--- src/at3/playerachievements/PdfGenerator.java
+++ src/at3/playerachievements/PdfGenerator.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 
 
 public class PdfGenerator {
@@ -16,9 +18,25 @@ public class PdfGenerator {
     public void generatePdf() 
     {
         try {
-            PDDocument doc = new PDDocument();
-            doc.save("C:/Users/Ben/Desktop/GitGub Projects/AT3_PlayerAchievements/testpdf.pdf");
-            doc.close();
+            PDDocument playerAchievementPdf = new PDDocument();
+            
+            //for loop to add 3 pages
+            for (int i = 0; i<3; i++)
+            {
+                PDPage newPage = new PDPage();
+                playerAchievementPdf.addPage(newPage);
+            }
+            String testWords = "Hi. This is test sentence.";
+            //add player 1 to first page
+            PDPage page1 = playerAchievementPdf.getPage(1);
+            PDPageContentStream contentStream = new PDPageContentStream(playerAchievementPdf, page1);
+            contentStream.beginText();
+            contentStream.showText(testWords);
+            contentStream.endText();
+            
+            //Save the document
+            playerAchievementPdf.save("C:/Users/Ben/Desktop/GitGub Projects/AT3_PlayerAchievements/testpdf.pdf");
+            playerAchievementPdf.close();
         } catch (IOException ex) {
             Logger.getLogger(PdfGenerator.class.getName()).log(Level.SEVERE, null, ex);
         }
