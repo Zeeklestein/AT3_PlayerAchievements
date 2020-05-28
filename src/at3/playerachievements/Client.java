4e3c91f22c7c119502e3e3a77bf476fcd7774288
Zeeklestein <bszekely91@gmail.com>
2020-05-28 13:04:10 +1000

    sendPlayer

--- src/at3/playerachievements/Client.java
+++ src/at3/playerachievements/Client.java
@@ -5,17 +5,42 @@
  */
 package at3.playerachievements;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author Ben
  */
 public class Client {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    Socket socket;
+    ObjectOutputStream outputStream;
+    
+    public void sendPlayer(Player player) throws IOException
+    {
+        socket = new Socket("local", 4445);
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+        outputStream.writeObject(player);
+        outputStream.flush();
+        outputStream.close();
+    }
+    
+    public void main(String[] args) throws IOException {
+        
+        ReadData readData = new ReadData();
+        readData.filePath("/Users/Ben/Desktop/GitGub Projects/AT3_PlayerAchievements/AT3CSV");
+        Player newPlayer = new Player();
+        
+        newPlayer = readData.readFilePlayers(1);
+        sendPlayer(newPlayer);
+        newPlayer = readData.readFilePlayers(2);
+        sendPlayer(newPlayer);
+        newPlayer = readData.readFilePlayers(3);
+        sendPlayer(newPlayer);
+        
+        sendPlayer(null);
     }
     
 }
